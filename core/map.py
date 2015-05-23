a5c27608b142d94c62839d8d6d66e4232744752d
kpace <krasimirva@gmail.com>
2015-05-23 15:54:50 +0300

    Map is printing properly.

--- core/map.py
+++ core/map.py
@@ -1,2 +1,42 @@
 class Map:
-    pass
+    def __init__(self, file_name):
+        self.__load_map(file_name)
+
+    def __load_map(self, file_name):
+        self.field = []
+        with open(file_name) as file:
+            y = 0
+            for line in file:
+                l = []
+                for x in range(0, len(line)):
+                    l.append(Cell(self, line[x], Position(x, y)))
+                self.field.append(l)
+                y += 1
+
+    def __str__(self):
+        s = ''
+        for line in self.field:
+            for cell in line:
+                s += str(cell)
+
+        return s
+
+
+class Cell:
+    def __init__(self, map, symbol, position):
+        self.map = map
+        self.symbol = symbol
+        self.position = position
+
+    def __str__(self):
+        return self.symbol
+
+
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+map = Map('static/maps/m1.txt')
+print(map)
