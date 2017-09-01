89b30fcd7481c452fe36cb4ed04c105b84bfaac6
kpace <krasimirva@gmail.com>
2017-09-01 19:47:33 +0300

    Rename directory tests to test

--- test/test_map.py
+++ test/test_map.py
@@ -5,13 +5,13 @@ from core.cells import Cell, Position
 
 class TestMap(unittest.TestCase):
     def test_getitem(self):
-        map = Map('../maps/test-map1.txt')
+        map = Map('maps/test-map1.txt')
         self.assertEqual(map[Position(0, 0)].symbol, '#')
         self.assertEqual(map[Position(1, 1)].symbol, ' ')
         self.assertEqual(map[Position(6, 3)].symbol, 'H')
 
     def test_getitem_bad_index(self):
-        map = Map('../maps/test-map1.txt')
+        map = Map('maps/test-map1.txt')
         self.assertRaises(
                 IndexError,
                 map.__getitem__,
@@ -26,7 +26,7 @@ class TestMap(unittest.TestCase):
                 Position(100, 100))
 
     def test_setitem(self):
-        map = Map('../maps/test-map1.txt')
+        map = Map('maps/test-map1.txt')
         cell = Cell(0, 0, symbol='!')
         map[Position(0, 0)] = cell
         map[Position(map.height - 1, map.width - 1)] = cell
@@ -34,7 +34,7 @@ class TestMap(unittest.TestCase):
         self.assertEqual(map[Position(map.height - 1, map.width - 1)], cell)
 
     def test_neighbours(self):
-        map = Map('../maps/test-map1.txt')
+        map = Map('maps/test-map1.txt')
         neighbours = map.neighbours(map[Position(1, 2)])
         self.assertEqual(neighbours[0], map[Position(0, 2)])
         self.assertEqual(neighbours[1], map[Position(1, 3)])
@@ -42,7 +42,7 @@ class TestMap(unittest.TestCase):
         self.assertEqual(neighbours[3], map[Position(1, 1)])
 
     def test_dist_between(self):
-        map = Map('../maps/test-map1.txt')
+        map = Map('maps/test-map1.txt')
         self.assertEqual(1, map.dist_between(
             map[Position(0, 0)], map[Position(0, 1)]))
         self.assertEqual(15, map.dist_between(
@@ -53,7 +53,7 @@ class TestMap(unittest.TestCase):
             map[Position(1, 0)], map[Position(5, 10)]))
 
     def test_swap_cells(self):
-        map = Map('../maps/test-map1.txt')
+        map = Map('maps/test-map1.txt')
         c1 = map[Position(6, 2)]
         c2 = map[Position(6, 3)]
         map.swap_cells(c1, c2)
